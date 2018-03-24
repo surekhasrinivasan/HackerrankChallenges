5059413ebf515246d486374a90afabaa5b08a55f
surekha <surekhasrao@gmail.com>
2018-03-24 13:32:46 +0000

    task3 algorithm solved

--- task3.js
+++ task3.js
@@ -5,9 +5,9 @@
 // Given an integer, perform the following conditional actions:
 
 // If n is odd, print Weird
-// If n is even and in the inclusive range of  to , print Not Weird
-// If n is even and in the inclusive range of  to , print Weird
-// If n is even and greater than , print Not Weird
+// If n is even and in the inclusive range of 2 to 5, print Not Weird
+// If n is even and in the inclusive range of 6 to 20, print Weird
+// If n is even and greater than 20, print Not Weird
 // Complete the stub code provided in your editor to print whether or not  is weird.
 
 // Input Format
@@ -63,6 +63,21 @@ function readLine() {
 }
 
 function main() {
-    var N = parseInt(readLine());
-
+    var n = parseInt(readLine());
+    var str="";
+          
+      // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+      if(n%2==1){
+         str = "Weird";
+      }
+      else if((n%2==0) && (n>=2 && n<=5)){
+              str ="Not Weird";
+          }
+          else if((n%2==0) && (n>=6 && n<=20)){
+              str="Weird";
+          }
+          else if((n%2==0) && (n>20)){
+              str ="Not Weird";
+          }    
+        console.log(str);   
 }
