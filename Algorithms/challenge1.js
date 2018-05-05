ba71dacd57f54609cfc72e817507a080ea623a3f
surekha <surekhasrao@gmail.com>
2018-05-05 15:49:07 +0000

    Hackerrank challenges

---
+++ Algorithms/challenge1.js
@@ -0,0 +1,55 @@
+// Complete the function solveMeFirst to compute the sum of two integers.
+
+// Function prototype:
+
+// int solveMeFirst(int x, int y);
+
+// where,
+
+// x is the first integer input.
+// y is the second integer input
+// Return values
+
+// sum of the above two integers
+// Sample Input
+
+// x = 2
+// y = 3
+// Sample Output 5
+
+// Explanation
+
+// The sum of the two integers  and  is computed as: 2 + 3 = 5
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+
+    input_stdin += data;
+});
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function solveMeFirst(a, b) {
+  // Hint: Type return a+b below   
+    return a + b;
+}
+
+function main() {
+    var a = parseInt(readLine());
+    var b = parseInt(readLine());;
+
+    var res = solveMeFirst(a, b);
+    console.log(res);
+}
